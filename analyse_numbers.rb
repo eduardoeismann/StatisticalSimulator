e150790cdb5e8bf4644fb6ac337dfac64716bdeb
Eduardo Eismann <eduardo.eismann@icloud.com>
2018-03-06 21:04:30 -0300

    Arithmetic average added, with a 10.times loop.

--- analyse_numbers.rb
+++ analyse_numbers.rb
@@ -1,28 +1,36 @@
 #MM
-print ("Codigo de matricula: ") # Registration code
+print "Codigo de registro: "     # Registration code
 registration = gets.chomp()
 
-print ("Valor maximo da geracao: ") # Max value to generate numbers between 0 and the number you inform
+print "Valor maximo da geracao: " # Max value to generate numbers between 0 and the number you inform
 maximum = gets.chomp()
 
-rejected = 0      # Incorrect numbers generated during the process
-reformulated = "" # To compose the registration again
-x = 0             # Index to control hits
+sampling = Array.new   # It'll stores all the final values generated to each register
 
-while x != registration.length
-	number = rand( Integer( maximum ) )
+# This three variables will be analysed at the end of the 10.times loop
+average = 0
+weigthed_average = 0   # Not implemented yet
+standart_deviation = 0 # Not implemented yet
+
+10.times {
+	rejected = 0      # Incorrect numbers generated during the process
+	x = 0             # Index to control hits
 	
-	if number == Integer( registration[ x ] )
-		x += 1
-		reformulated += number.to_s
-	else
-		rejected += 1
+	while x != registration.length
+		number = rand( Integer( maximum ) + 1 )
+		
+		if number == Integer( registration[ x ] )
+			x += 1
+		else
+			rejected += 1
+		end
+		
 	end
 	
-end
+	sampling[ x ] = rejected # Rejected numbers, that wasn't match with the original
+	
+	average += Integer( sampling[ x ] )
+	
+}
 
-print ("Numeros rejeitados: " ) # Rejected numbers, that wasn't match with the original
-puts rejected
-
-print ("Numero: " ) # The original number informed at the begin
-puts reformulated
+puts "Media encontrada: " + ( average / 10 ).to_s
